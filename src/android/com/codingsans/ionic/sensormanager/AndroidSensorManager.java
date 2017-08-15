6054660d59e6148832e0a89907551b30629f359c
Laszlo Csele <lacka@codingsans.com>
2017-08-15 13:31:46 +0200

    update deprecated methods

--- src/android/com/codingsans/ionic/sensormanager/AndroidSensorManager.java
+++ src/android/com/codingsans/ionic/sensormanager/AndroidSensorManager.java
@@ -11,6 +11,7 @@ import org.json.JSONException;
 
 import android.content.Context;
 import android.hardware.SensorManager;
+import android.hardware.SensorEventListener;
 import android.hardware.SensorEvent;
 import android.hardware.Sensor;
 
@@ -39,17 +40,23 @@ public class AndroidSensorManager extends CordovaPlugin {
     }
 
     public void onResume(boolean multitasking){
-        mSensorManager.registerListener(this.cordova.getActivity(), mSensor, SensorManager.SENSOR_DELAY_NORMAL);
+        mSensorManager.registerListener(listener, mSensor, SensorManager.SENSOR_DELAY_NORMAL);
     }
 
     public void onPause(boolean multitasking) {
-        mSensorManager.unregisterListener(mSensor);
+        mSensorManager.unregisterListener(listener);
     }
 
-    public void onSensorChanged(SensorEvent event) {
-        if (this.callbackContext != null) {
-            PluginResult result = new PluginResult(PluginResult.Status.OK, event.values[0]);
-            this.callbackContext.sendPluginResult(result);
+    private SensorEventListener listener = new SensorEventListener() {
+        public void onSensorChanged(SensorEvent event) {
+            if (this.callbackContext != null) {
+                PluginResult result = new PluginResult(PluginResult.Status.OK, event.values[0]);
+                this.callbackContext.sendPluginResult(result);
+            }
         }
-    }
+        
+        public void onAccuracyChanged(Sensor sensor, int accuracy) {
+            // unused
+        }
+    };
 }
